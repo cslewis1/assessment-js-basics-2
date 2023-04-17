67135cff118222f169fc7a5a534450a6170749b8
Charneaka Lewis <clewis20@ggc.edu>
2023-04-16 23:24:23 -0400

    Adding extra credit

---
+++ extra-credit.js
@@ -0,0 +1,23 @@
+/* 
+https://docs.google.com/document/d/1VzWfnvmw56dbZWm9HrWErLsteAdkqScIG1WiEdQeRgY/edit
+
+DocuSign:
+ C#, .NET, Java, NodeJS, JavaScript, SQL & NoSQL, Cassandra, 
+ TypeScript, React, Angular, HTML, CSS, AJAX, jQuery, 
+ Google Analytics, Kubernetes, Azure, Bandwidth, VB.Net
+
+
+Salesforce:
+	Java, Javascript, C++, C#, Ruby, Python, PHP, 
+    NodeJS, Express, Hapi, JavaScript, CSS, HTML5, 
+    XML, JSON, Ajax, jQuery, Google Analytics, 
+    GO1, Paylocity, Walkme, Origin Learning, Achievers, 
+    Database: PL/SQL, SQL Server, MySQL, PostgreSQL, Cassandra, 
+    MongoDB, Kubernetes and Docker
+
+ServiceNow:
+Java, NodeJS, JavaScript, Ruby, Python, SQL & NoSQL, Cassandra,React, 
+Angular, Vue HTML, CSS, AJAX, Javascript, jQuery, Prototype, 
+Google Analytics, Microsoft Azure, Golang, Kubernetes 
+
+*/
